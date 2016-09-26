694a2f25ce58c3ebc35c5a693651651aef33dd21
AlenaFedotova <nyonk@mail.ru>
2016-09-26 21:38:16 +0300

    Update rpn.pl

--- homeworks/calculator/lib/rpn.pl
+++ homeworks/calculator/lib/rpn.pl
@@ -24,10 +24,78 @@ require "$FindBin::Bin/../lib/tokenize.pl";
 sub rpn {
 	my $expr = shift;
 	my $source = tokenize($expr);
+
+	return rpn_($source);
+}
+
+sub rpn_ {
 	my @rpn;
+	$#rpn = -1;
 
-	# ...
-
+	my $x = $_[0];
+	
+	my $i;
+	my $p;
+	if($#$x==0) {return $x}
+	if ($x->[0] eq '(') {
+		for ($p=1, $i=1;$i<$#$x+1 && $p != 0;$i++) {
+			if ($x->[$i] eq '(') {$p++}
+			if ($x->[$i] eq ')') {$p--}
+		}
+		if ($p==0 && $i == $#$x+1) {
+			pop(@$x);
+			shift(@$x);
+		}
+	}
+	my $done=0;
+	if ($done==0) {
+		for($i=$#$x,$p=0; $i!=0 && !($p==0 && ($x->[$i] eq '+' || $x->[$i] eq '-'));$i--) {
+			if ($x->[$i] eq '(') {$p++}
+			if ($x->[$i] eq ')') {$p--}
+		}
+		if ($i!=0) {
+			$done++;
+			my @arr1=@{$x}[0..$i-1];
+			my @arr2=@{$x}[$i+1..$#$x];
+			@rpn = (@{rpn_(\@arr1)},@{rpn_(\@arr2)},$x->[$i]);
+		}
+	}
+	if ($done==0) {
+		for($i=$#$x,$p=0; $i!=0 && !($p==0 && ($x->[$i] eq '*' || $x->[$i] eq '/'));$i--) {
+			if ($x->[$i] eq '(') {$p++}
+			if ($x->[$i] eq ')') {$p--}
+		}
+		if ($i!=0) {
+			$done++;
+			my @arr1=@{$x}[0..$i-1];
+			my @arr2=@{$x}[$i+1..$#$x];
+			@rpn = (@{rpn_(\@arr1)},@{rpn_(\@arr2)},$x->[$i]);
+		}
+	}
+	if ($done==0) {
+		for($i=0,$p=0; $i!=$#$x && !($p==0 && ($x->[$i] eq '^'));$i++) {
+			if ($x->[$i] eq '(') {$p++}
+			if ($x->[$i] eq ')') {$p--}
+		}
+		if ($i!=$#$x) {
+			$done++;
+			my @arr1=@{$x}[0..$i-1];
+			my @arr2=@{$x}[$i+1..$#$x];
+			if ($arr1[0] eq 'U+' || $arr1[0] eq 'U-') {
+				my $y = $arr1[0];
+				shift (@arr1);
+				@rpn = (@{rpn_(\@arr1)},@{rpn_(\@arr2)},$x->[$i],$y);
+			}
+			else {
+				@rpn = (@{rpn_(\@arr1)},@{rpn_(\@arr2)},$x->[$i]);
+			}
+		}
+	}
+	if ($done==0) {
+		$done++;
+		my @arr1=@{$x}[1..$#$x];
+		@rpn = (@{rpn_(\@arr1)},$x->[0]);
+	}
 	return \@rpn;
 }
 
