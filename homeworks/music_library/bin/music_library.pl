d07a91502ad6a6818d027b801ecb3904d5dac62e
AlenaFedotova <nyonk@mail.ru>
2016-10-05 00:16:36 +0400

    Update music_library.pl

--- homeworks/music_library/bin/music_library.pl
+++ homeworks/music_library/bin/music_library.pl
@@ -2,6 +2,22 @@
 
 use strict;
 use warnings;
+use Local::MusicLibrary;
 
+my @col;
+$#col = -1;
+my %opt;
+Local::MusicLibrary::GetOpting(\%opt, \@col);
+
+my @arr;
+$#arr = -1;
 while (<>) {
+	my $tmp;
+	my %h;
+	$tmp = \%h;
+	Local::MusicLibrary::TakeArray $_, $tmp;
+	push @arr, {%h};
 }
+Local::MusicLibrary::GrepArray \@arr, \%opt;
+Local::MusicLibrary::SortArray \@arr, $opt{sort};
+Local::MusicLibrary::Print \@arr, \@col;
