ed421a9a2539f38313b064a796407716095a36c7
AlenaFedotova <nyonk@mail.ru>
2016-10-08 14:36:19 +0400

    Update music_library.pl

--- homeworks/music_library/bin/music_library.pl
+++ homeworks/music_library/bin/music_library.pl
@@ -2,21 +2,31 @@
 
 use strict;
 use warnings;
-use Local::MusicLibrary::Taking;
+use Getopt::Long;
 use Local::MusicLibrary::Processing;
 use Local::MusicLibrary::Printing;
 
-my @col;
-$#col = -1;
+my @col0 = ("band", "year", "album", "track", "format");
+my @sep = ("\/", ' - ', "\/", '\.', "\n");
+my %is_num;
+for (@col0) {$is_num{$_} = 0} $is_num{"year"} = 1;
 my %opt;
-Local::MusicLibrary::Taking::TakeOptions(\%opt);
+my @col;
+TakeOptions();
 
-my @arr;
-$#arr = -1;
+my @data;
 while (<>) {
 	my %h;
-	Local::MusicLibrary::Taking::TakeArray $_, \%h;
-	push @arr, {%h};
+	Local::MusicLibrary::Processing::TakeArray $_, \%h, \@col0, \@sep;
+	push @data, {%h};
 }
-Local::MusicLibrary::Processing::Process \@arr, \%opt;
-Local::MusicLibrary::Printing::Print \@arr, $opt{columns};
+Local::MusicLibrary::Processing::Process \@data, \%opt, \@col0, \@col, \%is_num;
+Local::MusicLibrary::Printing::Print \@data, \@col;
+
+sub TakeOptions {
+	for (@col0) {$opt{$_} = ""}
+	$opt{sort} = "";
+	$opt{columns} = -1;
+
+	Getopt::Long::GetOptions (\%opt, "$col0[0]=s", "$col0[1]=s", "$col0[2]=s", "$col0[3]=s", "$col0[4]=s", "sort=s", "columns:s");
+}
