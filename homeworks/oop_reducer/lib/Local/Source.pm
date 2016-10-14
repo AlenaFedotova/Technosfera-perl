10e21843c2ceab57caf711eb8a5fe13d9f862980
AlenaFedotova <nyonk@mail.ru>
2016-10-15 00:13:45 +0400

    Create Source.pm

---
+++ homeworks/oop_reducer/lib/Local/Source.pm
@@ -0,0 +1,35 @@
+package Local::Source;
+
+use strict;
+use warnings;
+
+=encoding utf8
+
+=head1 NAME
+
+Local::Source
+
+=head1 VERSION
+
+Version 1.00
+
+=cut
+
+our $VERSION = '1.00';
+
+=head1 SYNOPSIS
+
+=cut
+
+use parent qw(Local::Object);
+
+sub init {
+	my ($self) = @_;
+}
+
+sub next {
+	my ($self) = @_;
+	return;
+}
+
+1;
