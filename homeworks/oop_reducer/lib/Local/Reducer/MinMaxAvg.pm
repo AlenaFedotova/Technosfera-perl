dd1e8b0c5a3d065910a9f9efda53845f74c4b599
AlenaFedotova <nyonk@mail.ru>
2016-10-19 19:58:07 +0400

    Update MinMaxAvg.pm

--- homeworks/oop_reducer/lib/Local/Reducer/MinMaxAvg.pm
+++ homeworks/oop_reducer/lib/Local/Reducer/MinMaxAvg.pm
@@ -26,45 +26,24 @@ use Local::Reducer::MinMaxAvg::Reduced;
 
 sub init {
 	my ($self) = @_;
-	$self->{reduced} = $self->{initial_value};
 	$self->{now} = $self->{source}->next();
 	$self->{sum} = 0;
 	$self->{n} = 0;
-	$self->{min} = 'undef';
-	$self->{max} = 'undef';
 }
 
-sub reduce_n {
-	my ($self, $n) = @_;
-	while ($n > 0 && $self->{now} ne 'undef') {
-		$self->{n}++;
-		my $tmp = $self->{row_class}->new(str => $self->{now})->get($self->{field}, 0)+0;
-		$self->{sum} += $tmp;
-		if ($self->{min} eq 'undef' || $self->{min} > $tmp) {$self->{min} = $tmp} 
-		if ($self->{max} eq 'undef' || $self->{max} < $tmp) {$self->{max} = $tmp} 
-		$self->{now} = $self->{source}->next();
-		$n--;
-	}
-	return $self->reduced();
-}
-
-sub reduce_all {
+sub reduce_one {
 	my ($self) = @_;
-	while ($self->{now} ne 'undef') {
-		$self->{n}++;
-		my $tmp = $self->{row_class}->new(str => $self->{now})->get($self->{field}, 0)+0;
-		$self->{sum} += $tmp;
-		if ($self->{min} eq 'undef' || $self->{min} > $tmp) {$self->{min} = $tmp} 
-		if ($self->{max} eq 'undef' || $self->{max} < $tmp) {$self->{max} = $tmp} 
-		$self->{now} = $self->{source}->next();
-	}
-	return $self->reduced();
+	$self->{n}++;
+	my $tmp = $self->{row_class}->new(str => $self->{now})->get($self->{field}, 0)+0;
+	$self->{sum} += $tmp;
+	if (!defined($self->{min}) || $self->{min} > $tmp) {$self->{min} = $tmp} 
+	if (!defined($self->{max}) || $self->{max} < $tmp) {$self->{max} = $tmp} 
 }
 
 sub reduced {
 	my ($self) = @_;
 	if ($self->{n} == 0) {
-		return Local::Reducer::MinMaxAvg::Reduced->new(avg => 'undef', min => 'undef', max => 'undef')
+		return Local::Reducer::MinMaxAvg::Reduced->new(avg => undef, min => undef, max => undef)
 	}
 	else {
 		return Local::Reducer::MinMaxAvg::Reduced->new(avg => $self->{sum}/$self->{n}, min => $self->{min}, max => $self->{max})
