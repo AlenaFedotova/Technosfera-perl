7169985b9a6f93ac22c42f7c07e35ac5fd98150a
AlenaFedotova <nyonk@mail.ru>
2016-10-19 19:57:30 +0400

    Update MaxDiff.pm

--- homeworks/oop_reducer/lib/Local/Reducer/MaxDiff.pm
+++ homeworks/oop_reducer/lib/Local/Reducer/MaxDiff.pm
@@ -23,33 +23,11 @@ our $VERSION = '1.00';
 
 use parent qw(Local::Reducer);
 
-sub reduce_n {
-	my ($self, $n) = @_;
-	while ($n > 0 && $self->{now} ne 'undef') {
-		my $tmpobj = $self->{row_class}->new(str => $self->{now});
-		my $mbmax = $tmpobj->get($self->{top}, 0) - $tmpobj->get($self->{bottom}, 0);
-		if ($mbmax > $self->{reduced}) {$self->{reduced} = $mbmax}
-		$self->{now} = $self->{source}->next();
-		$n--;
-	}
-	return $self->{reduced};
-}
-
-sub reduce_all {
+sub reduce_one {
 	my ($self) = @_;
-	while ($self->{now} ne 'undef') {
-		my $tmpobj = $self->{row_class}->new(str => $self->{now});
-		my $mbmax = $tmpobj->get($self->{top}, 0) - $tmpobj->get($self->{bottom}, 0);
-		if ($mbmax > $self->{reduced}) {$self->{reduced} = $mbmax}
-		$self->{now} = $self->{source}->next();
-	}
-	return $self->{reduced};
+	my $tmpobj = $self->{row_class}->new(str => $self->{now});
+	my $mbmax = $tmpobj->get($self->{top}, 0) - $tmpobj->get($self->{bottom}, 0);
+	if ($mbmax > $self->{reduced}) {$self->{reduced} = $mbmax}
 }
-
-sub reduced {
-	my ($self) = @_;
-	return $self->{reduced};
-}
-
 
 1;
