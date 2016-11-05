1032652d66d11da83c4e2b1fc09a1803520fd63d
AlenaFedotova <nyonk@mail.ru>
2016-11-05 23:51:43 +0400

    Update Queue.pm

--- homeworks/net_calc/lib/Local/TCP/Calc/Server/Queue.pm
+++ homeworks/net_calc/lib/Local/TCP/Calc/Server/Queue.pm
@@ -13,11 +13,15 @@ use DDP;
 has f_handle       => (is => 'rw', isa => 'FileHandle');
 has queue_filename => (is => 'ro', isa => 'Str', default => '/tmp/local_queue.log');
 has max_task       => (is => 'rw', isa => 'Int', default => 0);
+has last_added_id  => (is => 'rw', isa => 'Int', default => 0);
+has last_given_id  => (is => 'rw', isa => 'Int', default => 0);
+has num_finish_id  => (is => 'rw', isa => 'Int', default => 0);
 
 sub init {
 	my $self = shift;
 	open($self->{f_handle}, '>', $self->{queue_filename})
 		or die "Can't open ".$self->{queue_filename}."\n";
+	$self->{f_handle}->print ($self->{last_added_id}.' '.$self->{last_given_id}.' '.$self->{num_finish_id}."\n");
 	close($self->{f_handle});
 }
 
@@ -27,39 +31,39 @@ sub open {
 
 	open($self->{f_handle}, $open_type, $self->{queue_filename})
 		or die "Can't open ".$self->{queue_filename}."\n";
-	until (flock($self->{f_handle}, 2)) {sleep 0.01}
-
+	until (flock($self->{f_handle}, LOCK_EX)) {sleep 0.01}
 	my $fh = $self->{f_handle};
+	my $first_line = <$fh>;
+	chomp $first_line;
+	($self->{last_added_id}, $self->{last_given_id}, $self->{num_finish_id}) = split / /, $first_line;
+
 	my @lines = <$fh>;
 	my %hash;
-	my @id;
 
 	for (@lines) {
 		chomp $_;
 		my @tmp = split /:/, $_;
 		$hash{$tmp[0]} = $tmp[1];
-		push(@id, $tmp[0]);
 	}
-	return [\%hash, \@id];
+
+	return \%hash;
 }
 
 sub close {
 	my $self = shift;
 	my $struct = shift;
 	my $open_type = shift;
+
 	if ($open_type ne '<') {
 		truncate($self->{f_handle}, 0);
 		seek($self->{f_handle}, 0, 0);
-		
-		my %hash = %{$struct->[0]};
-		my @id = @{$struct->[1]};
 
-		for (@id) {
-		say $_;	
-			$self->{f_handle}->print ($_.":".$hash{$_}."\n");
+		$self->{f_handle}->print ($self->{last_added_id}.' '.$self->{last_given_id}.' '.$self->{num_finish_id}."\n");
+		for my $id (1..$self->{last_added_id}) {	
+			$self->{f_handle}->print ($id.":".$struct->{$id}."\n");
 		}
 	}
-	flock($self->{f_handle}, 8)
+	flock($self->{f_handle}, LOCK_UN)
 		or die "Can't unlock ".$self->{queue_filename}."\n"; 
 
 	close($self->{f_handle});
@@ -71,20 +75,19 @@ sub to_done {
 	my $status = shift;
 	
 	my $struct = $self->open('+<');
-	my %hash = %{$struct->[0]};
-	$hash{$task_id} = $status;
+	$struct->{$task_id} = $status;
+	$self->{num_finish_id}++;
 
-	$self->close([\%hash, $struct->[1]], '+<');
+	$self->close($struct, '+<');
 }
 
 sub get_status {
 	my $self = shift;
 	my $id = shift;
 	my $struct = $self->open('<');
-	my %hash = %{$struct->[0]};
 	my $status = 'not found';
-	if (defined($hash{$id})) {
-		$status = $hash{$id};
+	if (defined($struct->{$id})) {
+		$status = $struct->{$id};
 	}
 	$self->close($struct, '<');
 
@@ -96,39 +99,31 @@ sub delete {
 	my $id = shift;
 	my $status = shift;
 	my $struct = $self->open('+<');
-	my %hash = %{$struct->[0]};
-	my @id = @{$struct->[1]};
-	my $real_status = $hash{$id};
+	my $real_status = $struct->{$id};
 	if ($status eq $real_status) {
-		delete $hash{$id};
-		my $i;
-		for ($i = 0; $id[$i] ne $id; $i++) {}
-		splice(@id, $i, 1);
+		$struct->{$id} = 'deleted';
 		unlink(Local::TCP::Calc::Server::FileName::task($id));
 		unlink(Local::TCP::Calc::Server::FileName::res($id));
 	}
-	$self->close([\%hash, \@id], '+<');
+	$self->close($struct, '+<');
 }
 
 sub get {
 	my $self = shift;
 
 	my $struct = $self->open('+<');
-
-	my %hash = %{$struct->[0]};
-	my @id = @{$struct->[1]};
-
 	my $tmp = 0;
 	my $id;
-	for (my $i = 0; $i <= $#id && !$tmp; $i++) {
-		if ($hash{$id[$i]} eq 'new') {
-			$hash{$id[$i]} = 'work';
-			$tmp++;
-			$id = $id[$i];
-		}
+
+
+	if ($self->{last_added_id} > $self->{last_given_id}) {
+		$self->{last_given_id}++;
+
+		$id = $self->{last_given_id};
+		$struct->{$id} = 'work';
 	}
 
-	$self->close([\%hash, \@id], '+<');
+	$self->close($struct, '+<');
 
 	if (defined($id)) {return $id}
 }
@@ -138,37 +133,36 @@ sub add {
 	my $new_work = shift;
 
 	my $struct = $self->open('+<');
-	my %hash = %{$struct->[0]};
-	my @id = @{$struct->[1]};
 	my $new_id;
-	if ($#id+1 < $self->{max_task}) {
-		if ($#id == -1) {$new_id = 1}
-		else {$new_id = $id[$#id]+1}
-		push(@id, $new_id);
-		$hash{$new_id} = 'new';
+
+	if ($self->{last_added_id} - $self->{num_finish_id} < $self->{max_task}) {
+		$self->{last_added_id}++;
+
+		$new_id = $self->{last_added_id};
+		$struct->{$new_id} = 'new';
 		my $filename = Local::TCP::Calc::Server::FileName::task($new_id);
-		open(my $fh, '>', $filename)
+		CORE::open(my $fh, '>', $filename)
 			or die "Can't open ".$filename."\n";
 		print $fh join("\n", @$new_work)."\n";
-		close($fh)
+		CORE::close($fh)
 			or die "Can't close ".$filename."\n";
 	}
 	else {$new_id = 0}
 
-	$self->close([\%hash, \@id], '+<');
+	$self->close($struct, '+<');
 	return $new_id;
 }
 
 sub delete_all {
 	my $self = shift;
 
-	my $struct = $self->open('+<');
-	my @id = @{$struct->[1]};
-	for (@id) {
+	my $struct = $self->open('<');
+
+	for my $id (1..$self->{last_added_id}) {
 		unlink(Local::TCP::Calc::Server::FileName::task($_));
 		unlink(Local::TCP::Calc::Server::FileName::res($_));
 	}
-	$self->close($struct, '+<');
+	$self->close({}, '<');
 	unlink($self->{queue_filename});
 }
 
