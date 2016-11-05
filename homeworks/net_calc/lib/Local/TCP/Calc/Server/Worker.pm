c729afba92b206e0c8204d7c555ddb2664146f8c
AlenaFedotova <nyonk@mail.ru>
2016-11-05 23:52:08 +0400

    Update Worker.pm

--- homeworks/net_calc/lib/Local/TCP/Calc/Server/Worker.pm
+++ homeworks/net_calc/lib/Local/TCP/Calc/Server/Worker.pm
@@ -9,12 +9,37 @@ use Fcntl qw(:flock);
 use POSIX;
 
 has cur_task_id => (is => 'ro', isa => 'Int', required => 1);
-has forks       => (is => 'rw', isa => 'HashRef', default => sub {return {}});
 has calc_ref    => (is => 'ro', isa => 'CodeRef', required => 1);
 has max_forks   => (is => 'ro', isa => 'Int', required => 1);
 
 my $err = 0;
 my $num_forks = 0;
+my $forks = {};
+
+sub REAPER {
+
+	while (my $pid = waitpid(-1, WNOHANG)) {
+
+		last if $pid == -1;
+		my $status = $?;
+		if ($status != 0) {
+			$err++;
+		}
+
+		delete $forks->{$pid};
+		$num_forks--;
+
+	}
+	$SIG{CHLD} = \&REAPER;
+}
+	
+
+sub int_signal {
+	for (%{$forks}) {
+		kill('TERM', $_);
+	}
+	exit(0);
+};
 
 sub write_err {
 	my $self = shift;
@@ -23,10 +48,10 @@ sub write_err {
 	my $fh;
 	open($fh, '>', Local::TCP::Calc::Server::FileName::res($self->{cur_task_id}))
 		or die "Can't open ".Local::TCP::Calc::Server::FileName::res($self->{cur_task_id})."\n";
-	until (flock($fh, 2)) {sleep 0.01}
+	until (flock($fh, LOCK_EX)) {sleep 0.01}
 
 	say $fh $error;
-	flock($fh, 8)
+	flock($fh, LOCK_UN)
 		or die "Can't unlock ".Local::TCP::Calc::Server::FileName::res($self->{cur_task_id})."\n";
 	close($fh);
 }
@@ -39,10 +64,10 @@ sub write_res {
 
 	open($fh, '>>', Local::TCP::Calc::Server::FileName::res($self->{cur_task_id}))
 		or die "Can't open ".Local::TCP::Calc::Server::FileName::res($self->{cur_task_id})."\n";
-	until (flock($fh, 2)) {sleep 0.01}
+	until (flock($fh, LOCK_EX)) {sleep 0.01}
 
 	say $fh $task.' == '.$res;
-	flock($fh, 8)
+	flock($fh, LOCK_UN)
 		or die "Can't unlock ".Local::TCP::Calc::Server::FileName::res($self->{cur_task_id})."\n";
 	close($fh);
 }
@@ -50,33 +75,7 @@ sub write_res {
 sub start {
 	my $self = shift;
 
-
-
-	sub REAPER {
-		while (my $pid = waitpid(-1, WNOHANG)) {
-
-			last if $pid == -1;
-
-			my $status = $?;
-			if ($status != 0) {
-				$err++;
-			}
-
-			delete $self->forks->{$pid};
-			$num_forks--;
-
-		}
-		$SIG{CHLD} = \&REAPER;
-	}
-	
-
-	$SIG{INT} = sub {
-		for (%{$self->forks}) {
-			kill('TERM', $_);
-		}
-		exit(0);
-	};
-
+	$SIG{INT} = \&int_signal;
 	my $fhtask;
 	open($fhtask, '<', Local::TCP::Calc::Server::FileName::task($self->{cur_task_id}))
 		or die "Can't open ".Local::TCP::Calc::Server::FileName::task($self->{cur_task_id})."\n";
@@ -84,7 +83,7 @@ sub start {
 	while (<$fhtask>) {
 		chomp $_;
 		if ($err) {
-			for (%{$self->forks}) {
+			for (%{$forks}) {
 				kill('TERM', $_);
 			}
 			last
@@ -96,7 +95,7 @@ sub start {
 		my $child = fork();
 		if ($child) {
 			$num_forks++;
-			$self->forks->{$child} = $child;
+			$forks->{$child} = $child;
 			$SIG{CHLD} = \&REAPER;
 			next;
 		}
@@ -105,11 +104,11 @@ sub start {
 
 			$self->write_res($_, $res);
 			exit(0);
+			
 		}
 		else {die "Can't fork: $!"}
 	}
 	while ($num_forks) {sleep 0.1}
-
 	close $fhtask;
 	return $err;
 }
