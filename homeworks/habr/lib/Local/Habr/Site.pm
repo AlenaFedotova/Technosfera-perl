ce03ac939acc163b16b7e75002a41a80a537b84a
AlenaFedotova <nyonk@mail.ru>
2016-11-14 00:25:41 +0400

    Create Site.pm

---
+++ homeworks/habr/lib/Local/Habr/Site.pm
@@ -0,0 +1,132 @@
+package Local::Habr::Site;
+
+use strict;
+use warnings;
+use LWP::UserAgent;
+use Mouse;
+use open ":utf8",":std";
+use Mojo::DOM;
+use feature 'say';
+use DDP;
+use Lingua::Translit;
+use List::MoreUtils;
+
+=encoding utf8
+
+=head1 NAME
+
+Local::Habr - habrahabr.ru crawler
+
+=head1 VERSION
+
+Version 1.00
+
+=cut
+
+our $VERSION = '1.00';
+
+=head1 SYNOPSIS
+
+=cut
+
+has address => (is => 'ro', isa => 'Str', default => 'https://habrahabr.ru/');
+has ua => (is => 'rw', isa => 'LWP::UserAgent');
+
+sub init {
+	my $self = shift;
+
+	$self->{ua} = LWP::UserAgent->new;
+	$self->{ua}->timeout(10);
+	$self->{ua}->env_proxy;
+}
+
+sub take_user {
+	my $self = shift;
+	my $name = shift;
+
+	my $response = $self->{ua}->get($self->{address}.'users/'.$name.'/');
+	if (!$response->is_success) {return undef}
+	my $str = $response->decoded_content;
+
+	my $dom = Mojo::DOM->new($str);
+	my $username = $dom->at('a[class="author-info__nickname"]')->text;
+	$username = substr($username, 1);
+	my $rating = $dom->at('div[class="statistic__value statistic__value_magenta"]')->text;
+	my $karma = $dom->at('div[class="voting-wjt__counter-score js-karma_num"]')->text;
+
+	$karma =~ s/\,/\./;
+	$rating =~ s/\,/\./;
+
+	if (!($karma =~ /^[\d\.\-]*$/)) {
+		$karma =~ s/^.//;
+		$karma =~ s/^/-/;
+	}
+	if (!($rating =~ /^[\d\.\-]*$/)) {
+		$rating =~ s/^.//;
+		$rating =~ s/^/-/;
+	}
+	
+	return {username => $username, karma => $karma, rating => $rating}
+}
+
+sub take_company {
+	my $self = shift;
+	my $name = shift;
+
+	return {username => $name};
+}
+
+sub take_post {
+	my $self = shift;
+	my $id = shift;
+
+	my $response = $self->{ua}->get($self->{address}.'post/'.$id.'/');
+	if (!$response->is_success) {return undef}
+	my $str = $response->decoded_content;
+
+	my $dom = Mojo::DOM->new($str);
+	my $post_id = $dom->at('input[name="moderation"]')->{value};
+	my $author = $dom->at('a[class="author-info__nickname"]');
+	if (!defined($author)) {
+		$author = $dom->at('a[class="author-info__name"]')->text;
+		$author = $self->take_company($author);
+	}
+	else {
+		$author = $author->text;
+		$author = substr($author, 1);
+		$author = $self->take_user($author)
+	}
+	
+	my $views = $dom->at('div[class="views-count_post"]')->text;
+	my $stars = $dom->at('span[class="favorite-wjt__counter js-favs_count"]')->text;
+	my $topic = $dom->at('meta[property="og:title"]')->{content};
+	my $tmp = $dom->at('span[class="voting-wjt__counter-score js-score"]');
+	my $rating;
+	if (defined($tmp)) {$rating = $tmp->text}
+
+	my @commenters;
+	for my $e ($dom->find('span[class="comment-item__user-info"]')->each) {
+		my $name = $e->{'data-user-login'};
+		if (!List::MoreUtils::any {$_->{username} eq $name} @commenters) {
+			push(@commenters, $self->take_user($name))
+		}
+	}
+	if (defined $rating) {
+		$rating =~ s/\,/\./;
+		$rating =~ s/^\+//;
+		if (!($rating =~ /^[\d\.\-]*$/)) {
+			$rating =~ s/^.//;
+			$rating =~ s/^/-/;
+		}
+	}
+	else {$rating = 0}
+
+	my $tr = new Lingua::Translit("GOST 7.79 RUS");
+	if ($tr->can_reverse()) {
+		$topic = $tr->translit($topic)
+	}
+
+	return ({id => $post_id, author => $author->{username}, topic => $topic, rating => $rating+0, views => $views, stars => $stars}, $author, \@commenters)
+}
+
+1;
