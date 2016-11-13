f311488d32634c6a64cfe4ee21e0b90a98c736c9
AlenaFedotova <nyonk@mail.ru>
2016-11-14 00:25:20 +0400

    Create Cache.pm

---
+++ homeworks/habr/lib/Local/Habr/Cache.pm
@@ -0,0 +1,52 @@
+package Local::Habr::Cache;
+
+use strict;
+use warnings;
+use Cache::Memcached::Fast;
+use Mouse;
+use feature 'say';
+use DDP;
+
+=encoding utf8
+
+=head1 NAME
+
+Local::Habr - habrahabr.ru crawler
+
+=head1 VERSION
+
+Version 1.00
+
+=cut
+
+our $VERSION = '1.00';
+
+=head1 SYNOPSIS
+
+=cut
+
+has memd => (is => 'rw', isa => 'Cache::Memcached::Fast');
+has server => (is => 'ro', isa => 'Str', default => '127.0.0.1:11212');
+
+
+sub init {
+	my $self = shift;
+
+	$self->{memd} = Cache::Memcached::Fast->new({servers => [{address => $self->{server}}]});
+}
+
+sub take_user {
+	my $self = shift;
+	my $user = shift;
+
+	return $self->{memd}->get($user)
+}
+
+sub add_user {
+	my $self = shift;
+	my $user = shift;
+
+	if (!$self->{memd}->set($user->{username}, $user)) {say ':-('}
+}
+
+1;
